cd1cc99d7439ab23a5bf3a72310591ca1e3362e9
Sakif Zaman <sm659604@dal.ca>
2019-05-23 21:37:41 -0300

    did some stuff

---
+++ headers/sortTokens.hpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<string> sortedTokens(string fileName) {
+  ifstream file(fileName);
+  string line;
+  list<string> tokens;
+  while (getline(file, line)) {
+    // cout << line << endl;
+    tokens.push_back(line);
+  }
+  file.close();
+  tokens.sort();
+  return tokens;
+}
--- src/lab2.cpp
+++ src/lab2.cpp
@@ -1,7 +1,7 @@
+#include "sortTokens.hpp"
 #include "splitString.hpp"
 #include <fstream>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -15,22 +15,31 @@ int main(int argc, char *argv[]) {
   }
 
   string line;
-  double comaCount = 0.0;
+  auto comaCount = 0.0;
+  auto sheCount = 0U;
 
   while (getline(readFile, line)) {
     // cout << line << endl;
     auto tokens = resplit(line);
     while (!tokens.empty()) {
-      auto tkn = tokens.front();
-      auto found = tkn.find(",");
-      if (found != string::npos)
-        comaCount += 1.0;
-      writeFile << tkn << "\t" << tkn.length() << endl;
-      tokens.pop_front();
+      auto tkn = tokens.front();              /* get the first token */
+      auto comaFound = tkn.find(",");         /* search for coma in the token */
+      comaCount += comaFound != string::npos; /* if not npos means there is a coma in toke */
+
+      auto sheFount = tkn.find("she");
+      sheCount += sheFount != string::npos;
+      sheFount = tkn.find("She");
+      sheCount += sheFount != string::npos;
+
+      writeFile << tkn << "\t" << tkn.length() << endl; /* add the token to log file along with its length */
+      tokens.pop_front();                               /* remove the token from list */
     }
   }
 
-  writeFile << "Coma Count: " << scientific << comaCount << endl;
+  printf("Coma found: %f\n", comaCount);
+  printf("she+She found: 0x%x\n", sheCount);
+  auto sortMe = sortedTokens("SortMe.txt").front(); /* should return Lorem a L is 76 wh */
+  cout << "First token to sort: " << sortMe << endl;
   readFile.close();
   writeFile.close();
   return 0;
