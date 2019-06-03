ac30648804651628356d3230f737313da4d22a71
Sakif Fahmid Zaman <smfzaman@gmail.com>
2019-06-02 22:07:55 -0300

    symbols

---
+++ compArchA1/headers/symbol.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+
+#ifndef SYMBOL_TYPE
+#define SYMBOL_TYPE
+enum symbolType { UNKNOWN,
+                  REGISTER,
+                  LABEL };
+
+#endif
+
+#ifndef SYMBOL
+#define SYMBOL
+class symbol {
+public:
+  string name;
+  short value;
+  symbolType type;
+  symbol *next;
+
+  symbol(string, short, symbolType, symbol * = nullptr);
+  void update(short, symbolType);
+};
+
+symbol::symbol(string n, short v, symbolType t, symbol *nextSymbol) : name(n), value(v), type(t), next(nextSymbol) {}
+
+void symbol::update(short v, symbolType t) {
+  value = v;
+  type = t;
+}
+
+#endif
