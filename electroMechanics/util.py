a1088dafbec9ae1518f750f81c00785a91949440
Sakif Zaman <smfzaman@gmail.com>
2019-09-07 23:21:06 -0300

    power

--- electroMechanics/util.py
+++ electroMechanics/util.py
@@ -2,6 +2,20 @@ import numpy as np
 
 
 def phaserString(z):
-    mag = np.absolute(z)
+    mag = np.abs(z)
     phase = np.angle(z, deg=True)
     return f"{mag:.4}∠{phase:.4}°"
+
+
+def complexPower(v, i):
+    return v * np.conj(i)
+
+
+def powerFactor(v, i):
+    phaseV = np.angle(v)
+    phaseI = np.angle(i)
+    return np.cos(phaseV - phaseI)
+
+
+def realPower(v, i):
+    return np.abs(v) * np.abs(i) * powerFactor(v, i)
