72a1d639ac27622e1ccc9bcc14d21176120403bf
Sakif Zaman <smfzaman@gmail.com>
2019-09-15 17:12:49 -0300

    q5

--- electroMechanics/a1.py
+++ electroMechanics/a1.py
@@ -29,7 +29,7 @@ emf /= time
 print(f"emf: {emf:.4} V")
 
 lbl = " A1Q2 "
-print(f"{lbl:#^80}")
+print(f"\n{lbl:#^80}")
 
 lbl = " a "
 print(f"{lbl:=^80}")
@@ -41,3 +41,45 @@ print(f"mmf: {mmf:.4} ")
 
 lbl = " b "
 print(f"{lbl:=^80}")
+
+lbl = " A1Q3 "
+print(f"{lbl:#^80}")
+momentOfInertia = 4
+torque = 6
+t = 5
+
+lbl = " A1Q5 "
+print(f"\n{lbl:#^80}")
+lbl = " a "
+print(f"{lbl:=^80}")
+radius = 25e-2  # m
+area = 3 * pow(1 / 100, 2)  # cm^2
+turns = 600
+current = 1.5  # A
+rPermability = 1500
+
+meanLength = 2 * const.pi * radius
+print(f"mean length: {meanLength:.4} m")
+reluctance = meanLength
+reluctance /= rPermability * const.mu_0 * area
+print(f"reluctance: {reluctance:.4} H^-1")
+
+lbl = " b "
+print(f"{lbl:=^80}")
+mmf = turns * current
+print(f"mmf: {mmf:.4} A")
+fieldIntensity = mmf / meanLength
+print(f"magnetic field intensity: {fieldIntensity:.4} H m^-1")
+
+lbl = " c "
+print(f"{lbl:=^80}")
+flux = mmf / reluctance
+print(f"flux: {flux:.3e} Wb")
+fluxDensity = flux / area
+print(f"flux density: {fluxDensity:.4} T")
+
+lbl = " A1Q6 "
+print(f"\n{lbl:#^80}")
+
+lbl = " A1Q7 "
+print(f"\n{lbl:#^80}")
