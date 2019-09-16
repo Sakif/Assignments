1eb9504b3402d99e91fd9c381bdb7965a60cbcbd
Sakif Fahmid Zaman <smfzaman@gmail.com>
2019-09-16 15:30:25 -0300

    lab 1

---
+++ systemAnalysis/primeFactors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void primeFactors(int n){
+  cout<<"Factors: ";
+  while (n % 2 == 0){
+      cout << 2 << " ";
+      n = n/2;
+    }
+  for (auto i = 3U; i <= sqrt(n); i +=  2){
+      while (n % i == 0){
+	  cout << i << " ";
+	  n = n/i;
+	}
+    }
+  if (n > 2)
+    cout << n << " ";
+  cout<<endl;
+}  
+
+int main(){
+  auto n = 5;
+  primeFactors(n);
+  n=8;
+    primeFactors(n);
+    n=20;
+      primeFactors(n);
+  return 0;}
