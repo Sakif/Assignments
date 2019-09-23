7b9fc9fa62348d10f949c54e943272ada237dbef
Sakif Fahmid Zaman <smfzaman@gmail.com>
2019-09-23 15:53:36 -0300

    done lab 2

--- systemAnalysis/lab2task2.cpp
+++ systemAnalysis/lab2task2.cpp
@@ -1,5 +1,51 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() { return 0; }
+struct studentRecord {
+  string firstName;
+  string lastName;
+  char studyLevel;
+  char sex;
+  unsigned ID;
+  double gpa;
+};
+
+studentRecord *readRecordFile(string fileName, unsigned &counter) {
+  studentRecord *recordArray;
+  ifstream reader(fileName);
+  if (!reader) {
+    cout << "Error opening file: " << fileName << endl;
+    exit(1);
+  } else {
+    reader >> counter;
+    recordArray = new studentRecord[counter];
+    for (auto i = 0U; i < counter; i++) {
+      reader >> recordArray[i].firstName;
+      reader >> recordArray[i].lastName;
+      reader >> recordArray[i].gpa;
+      reader >> recordArray[i].studyLevel;
+      reader >> recordArray[i].sex;
+      reader >> recordArray[i].ID;
+    }
+  }
+  reader.close();
+  return recordArray;
+}
+
+int main() {
+  auto femaleCount = 0U, underGradCount = 0U, arraySize = 0U;
+  auto records = readRecordFile("student.txt", arraySize);
+  for (auto i = 0U; i < arraySize; i++) {
+    if (records[i].sex == 'F')
+      femaleCount++;
+    if (records[i].studyLevel == 'U')
+      underGradCount++;
+  }
+  cout << "There are " << femaleCount << " female students." << endl;
+  cout << "There are " << underGradCount << " undergrad students." << endl;
+  return 0;
+}
