96814a873ae7983e912a4b4650faa3f6a765f176
Sakif Zaman <smfzaman@gmail.com>
2019-10-01 13:35:35 -0300

    use-package

--- init.el
+++ init.el
@@ -2,28 +2,84 @@
 ;;; Code:
 (package-initialize)
 
+(use-package elpy
+  :init
+  (setq python-shell-interpreter "python3")
+  (setq elpy-rpc-python-command "python3")
+  :config
+  (elpy-enable)
+  :hook (before-save . elpy-format-code))
+
+(use-package company
+  :config
+  (global-company-mode t)
+  :bind ("M-/" . company-complete-common-or-cycle))
+
+(use-package irony
+  :hook
+  ((c++-mode . irony-mode)
+   (c-mode . irony-mode)
+   (irony-mode . irony-cdb-autosetup-compile-options))
+  :config
+  (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+
+(use-package flycheck
+  :config
+  (global-flycheck-mode t))
+
+(use-package helm-projectile
+  :init
+  (global-unset-key (kbd "C-x c"))
+  (setq helm-autoresize-max-height 50)
+  (setq helm-autoresize-min-height 30)
+  (setq helm-autoresize-mode t)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-ff-file-name-history-use-recentf t)
+  (setq helm-ff-search-library-in-sexp t)
+  (setq helm-mode-fuzzy-match t)
+  (setq helm-move-to-line-cycle-in-source t)
+  (setq helm-recentf-fuzzy-match t)
+  (setq helm-scroll-amount 8)
+  (setq helm-split-window-inside-p t)
+  :config
+  (setq helm-mode t)
+  (helm-projectile-on)
+  (projectile-mode +1)
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x b" . helm-mini)
+   ("M-y" . helm-show-kill-ring)
+   ("C-x C-f" . helm-find-files)))
+
+(use-package doom-modeline
+  :init
+  (setq doom-modeline-icon (display-graphic-p))
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-buffer-state-icon t)
+  (setq doom-modeline-buffer-file-name-style 'file-name)
+  (setq doom-modeline-buffer-modification-icon t)
+  (setq doom-modeline-minor-modes (featurep 'minions))
+  :hook (after-init . doom-modeline-mode))
+
+(use-package doom-themes
+  :config
+  (doom-themes-visual-bell-config)
+  (load-theme 'doom-one t))
+
+(use-package auto-package-update
+  :init
+  (setq auto-package-update-delete-old-versions t)
+  :config
+  (auto-package-update-maybe))
+
 (custom-set-variables
- '(auto-package-update-delete-old-versions t)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (tango-dark)))
- '(elpy-rpc-python-command "python3")
- '(global-company-mode t)
- '(global-flycheck-mode t)
  '(global-visual-line-mode t)
- '(helm-autoresize-max-height 50)
- '(helm-autoresize-min-height 30)
- '(helm-autoresize-mode t)
- '(helm-buffers-fuzzy-matching t)
- '(helm-echo-input-in-header-line t)
- '(helm-ff-file-name-history-use-recentf t)
- '(helm-ff-search-library-in-sexp t)
- '(helm-mode t)
- '(helm-mode-fuzzy-match t)
- '(helm-move-to-line-cycle-in-source t)
- '(helm-recentf-fuzzy-match t)
- '(helm-scroll-amount 8)
- '(helm-split-window-inside-p t)
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
  '(menu-bar-mode nil)
@@ -32,41 +88,10 @@
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://stable.melpa.org/packages/"))))
  '(package-enable-at-startup t)
- '(package-selected-packages (quote (flycheck-irony company-irony cmake-ide helm-projectile auto-package-update elpy)))
- '(python-shell-interpreter "python3")
+ '(package-selected-packages
+   (quote
+    (flycheck-irony use-package company-irony)))
  '(scroll-all-mode nil)
  '(show-paren-mode t))
 
-(custom-set-faces)
-
-; irony
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-(eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-
-; elpy
-(elpy-enable)
-(add-hook 'python-mode-hook '(lambda() (add-hook 'before-save-hook 'elpy-format-code)))
-
-; helm
-(global-unset-key (kbd "C-x c"))
-(global-set-key (kbd "M-/") 'company-complete-common-or-cycle)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(helm-projectile-on)
-
-; clang formating
-(load "/usr/share/emacs/site-lisp/site-start.d/clang-format.el")
-(add-hook 'c++-mode-hook '(lambda() (add-hook 'before-save-hook 'clang-format-buffer)))
-(add-hook 'c-mode-hook '(lambda() (add-hook 'before-save-hook 'clang-format-buffer)))
-
-; projectile
-(require 'projectile)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
-
-(auto-package-update-maybe)
 ;;; init.el ends here
