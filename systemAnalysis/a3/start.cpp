8e71486557dd13053b8c4502623e1f188c39b6b1
Sakif Fahmid Zaman <smfzaman@gmail.com>
2019-11-16 17:16:50 -0400

    input files

--- systemAnalysis/a3/start.cpp
+++ systemAnalysis/a3/start.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-int main() {
-  cout << "Hello, World!" << endl;
+int main(const int argc, const char** argv) {
+  cout << "Have " << argc << " arguments. They are:" << endl;
+  for (auto i = 1; i < argc; i++) cout << argv[i] << endl;
   return 0;
 }
