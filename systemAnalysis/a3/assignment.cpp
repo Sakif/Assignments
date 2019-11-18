ee0a63066dcad4d880a55f88666763b5d85bb3fb
Sakif Fahmid Zaman <smfzaman@gmail.com>
2019-11-18 15:51:15 -0400

    cat

--- systemAnalysis/a3/assignment.cpp
+++ systemAnalysis/a3/assignment.cpp
@@ -2,15 +2,33 @@
 
 using namespace std;
 
+bool validLIN(int param) {
+  if (param > 0 && param < 101) return true;
+  return false;
+}
+
+bool validMQP(int param) {
+  if (param > 0 && param < 11) return true;
+  return false;
+}
+
 assignment::assignment(string inFileName, string outFileName) {
   inFile.open(inFileName);
   outFile.open(outFileName);
+
   inFile >> parameterL;
+  if (!validLIN(parameterL)) throw 20;
   inFile >> parameterI;
+  if (!validLIN(parameterI)) throw 20;
   inFile >> parameterN;
+  if (!validLIN(parameterN)) throw 20;
+
   inFile >> parameterM;
+  if (!validMQP(parameterM)) throw 22;
   inFile >> parameterQ;
+  if (!validMQP(parameterQ)) throw 22;
   inFile >> parameterP;
+  if (!validMQP(parameterP)) throw 22;
 
   auto n = 0, s = 0;
   inFile >> n;
@@ -27,7 +45,7 @@ assignment::~assignment() {
 }
 
 void assignment::test() {
-  outFile << "Parameter L: " << parameterL << endl;
+  /* outFile << "Parameter L: " << parameterL << endl;
   outFile << "Parameter I: " << parameterI << endl;
   outFile << "Parameter N: " << parameterN << endl;
   outFile << "Parameter M: " << parameterM << endl;
@@ -35,5 +53,5 @@ void assignment::test() {
   outFile << "Parameter P: " << parameterP << endl;
 
   outFile << "n: " << packetSendTime.size() << endl;
-  for (auto i : packetSendTime) outFile << i << endl;
+  for (auto i : packetSendTime) outFile << i << endl; //*/
 }
